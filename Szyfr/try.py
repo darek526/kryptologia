2f45c3970162c52f3d3ee3e84c4eda8bc7df4f78
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2018-12-08 23:16:43 +0100

    Add files via upload

--- Szyfr/try.py
+++ Szyfr/try.py
@@ -1,9 +1,12 @@
 lista=[]
 for i in range(4):
-    while 1:
+    while 1:# pętla nie skończona można wpisac True ten sam efekt każda wartość różna od zera to też prawda
         try:
-            lista.append(int(input("Podaj bit: \t")))
-            break
+            a=lista.append(int(input("Wpisz 0 lub 1: \t")))
+            break#jeśli jest błąd wyśietla komunikat i wraca do petli while info
+            # wpisaniu 0 lub 1  jesli niema błędu break zamyka petle while i wraca do petli for
         except ValueError:
+            print ("Pomyliłeś się. Tylko 0 lub 1. Spróbuj jeszcze raz...")
+        except UnicodeError:
             print ("Pomyliłeś się. Tylko 0 lub 1. Spróbuj jeszcze raz...")
 print(lista)
